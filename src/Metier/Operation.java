f3beab7be3d29f4617753e56c53745b7d27cb5bf
Hounaida202 <etrichih@gmail.com>
2025-09-20 20:57:19 +0100

    l ajout des attributs pour la classe operation

--- src/Metier/Operation.java
+++ src/Metier/Operation.java
@@ -1,4 +1,12 @@
 package Metier;
 
+import java.time.LocalDateTime;
+
 public abstract class Operation {
+
+    protected int numero;
+    protected LocalDateTime date;
+    protected double montant;
+    protected String type;
+    private static int compteur = 1;
 }
