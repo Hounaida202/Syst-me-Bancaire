16a53180bc1072fce54f7594185bee8d99ef99fa
Hounaida202 <etrichih@gmail.com>
2025-09-20 20:59:42 +0100

    l ajout de constructeur

--- src/Metier/Operation.java
+++ src/Metier/Operation.java
@@ -9,4 +9,11 @@ public abstract class Operation {
     protected double montant;
     protected String type;
     private static int compteur = 1;
+
+    public Operation(double montant, String type) {
+        this.numero = compteur++;
+        this.date = LocalDateTime.now();
+        this.montant = montant;
+        this.type = type;
+    }
 }
