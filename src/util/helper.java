ea8e18d5fcc15d296f2dc3da7ad95505877dddbc
Hounaida202 <etrichih@gmail.com>
2025-09-20 21:51:01 +0100

    ajouter la methode fair retrait pour effectuer un retrai

--- src/util/helper.java
+++ src/util/helper.java
@@ -2,6 +2,7 @@ package util;
 
 
 import Metier.Compte;
+import Metier.Retrait;
 
 import java.util.Scanner;
 
@@ -42,6 +43,28 @@ public class helper {
     }
 
 
+    private void faireRetrait() {
+        System.out.println("\n effectuer un retrait");
 
+        String code = helper.lireString(scanner, "Code du compte : ");
+        Compte compte = trouverCompte(code);
+
+        if (compte == null) {
+            System.out.println("Compte non trouvé ");
+            return;
+        }
+
+        double montant = helper.lireDouble(scanner, "Montant a retirer : ");
+        String destination = helper.lireString(scanner, "Destination : ");
+
+        Retrait retrait = new Retrait(montant, destination);
+        boolean reussi = retrait.faireRetrait(compte);
+
+        if (reussi) {
+            System.out.println("Retrait effectué. Nouveau solde : " + compte.getSolde() + " dh");
+        } else {
+            System.out.println(" retrait impossible ");
+        }
+    }
 
 }
