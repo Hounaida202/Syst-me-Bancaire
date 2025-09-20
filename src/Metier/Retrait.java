865edd13d7afbe151c5ec5f8a65bc805e64d58b9
Hounaida202 <etrichih@gmail.com>
2025-09-20 21:09:48 +0100

    ajouter une methde faire retrait

--- src/Metier/Retrait.java
+++ src/Metier/Retrait.java
@@ -12,4 +12,24 @@ public class Retrait extends Operation{
         return destination;
     }
 
+    public boolean faireRetrait(Compte compte) {
+        double newsolde = compte.getSolde() - montant;
+        if (compte instanceof CompteCourant) {
+            CompteCourant cc = (CompteCourant) compte;
+            if (newsolde >= -cc.getDecouvert()) {
+                compte.setSolde(newsolde);
+                compte.getOperations().add(this);
+                return true;
+            }
+        } else {
+
+            if (newsolde >= 0) {
+                compte.setSolde(newsolde);
+                compte.getOperations().add(this);
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
