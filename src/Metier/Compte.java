52522ade4ebdb44fabb217d67c9c00e33928fcd1
Hounaida202 <etrichih@gmail.com>
2025-09-20 20:26:45 +0100

    la creation du constructeur de la classe compte

--- src/Metier/Compte.java
+++ src/Metier/Compte.java
@@ -1,9 +1,18 @@
 package Metier;
 
+import java.util.ArrayList;
+
 public abstract class Compte {
     protected String code;
     protected double solde;
     protected ArrayList<Operation> operations;
     protected String type;
     private static int compteur = 1;
+
+    public Compte(double solde, String type) {
+        this.code = "CPT-" + String.format("%05d", compteur++);
+        this.solde = solde;
+        this.operations = new ArrayList<>();
+        this.type = type;
+    }
 }
