bc773f2f3848c1771fd46870a0b3a600128135c6
Hounaida202 <etrichih@gmail.com>
2025-09-20 20:41:16 +0100

    la redifinition de la methode de lajout du compte compatible selon le type de compte courant

--- src/Metier/CompteCourant.java
+++ src/Metier/CompteCourant.java
@@ -1,9 +1,16 @@
 package Metier;
 
+import java.util.ArrayList;
+
 public class CompteCourant extends Compte {
 
     private static final double decouvert = 200;
     public CompteCourant(double solde) {
         super(solde, "Courant");
+    }
+
+    @Override
+    public void ajouterCompte(ArrayList<Compte> comptes) {
+        comptes.add(this);
     }
 }
