13364b2a1815dfc1878f4220468677e54853701b
Hounaida202 <etrichih@gmail.com>
2025-09-20 21:36:25 +0100

    la creation d une methode pour verifie les choix

--- src/util/helper.java
+++ src/util/helper.java
@@ -1,7 +1,24 @@
 package util;
 
 
+import java.util.Scanner;
+
 public class helper {
 
-
+    public static int verifierchoix(Scanner scanner) {
+        int choix;
+        while (true) {
+            System.out.print("Choix entre 1 et 7: ");
+            if (scanner.hasNextInt()) {
+                choix = scanner.nextInt();
+                scanner.nextLine();
+                if (choix >= 1 && choix <= 7) {
+                    return choix;
+                }
+            } else {
+                scanner.nextLine();
+            }
+            System.out.println("choix invalide");
+        }
+    }
 }
