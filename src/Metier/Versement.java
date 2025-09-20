220edcd7eeb8818d3350866572205ead18fa089b
Hounaida202 <etrichih@gmail.com>
2025-09-20 21:12:08 +0100

    ajouter un constructeur

--- src/Metier/Versement.java
+++ src/Metier/Versement.java
@@ -3,5 +3,8 @@ package Metier;
 public class Versement extends Operation {
     private String source;
 
-
+    public Versement(double montant, String source) {
+        super(montant, "versement");
+        this.source = source;
+    }
 }
